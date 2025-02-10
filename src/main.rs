95f7695b12e58d0aca54f7c1d97b1a62c1c1dd34
Joshua Klein <mobiusklein@gmail.com>
2025-02-10 12:16:30 -0500

    feature: add more offset to main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::env;
 
-use log::info;
-
 use masslynx::reader::MassLynxReader;
 use masslynx::{self, MassLynxError};
 
 fn main() -> Result<(), MassLynxError> {
     pretty_env_logger::init_timed();
     let version = masslynx::get_mass_lynx_version();
-    info!("Using MassLynx Version: {:?}", version);
+    eprintln!("Using MassLynx Version: {:?}", version);
     let path = env::args().skip(1).next().unwrap();
-    info!("Opening {path}");
+    let spectrum_idx = env::args()
+        .skip(2)
+        .next()
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or_default();
+    eprintln!("Opening {path}");
     let mut reader = MassLynxReader::from_path(&path)?;
-    info!("Opened reader with {} spectra", reader.len());
+    eprintln!("Opened reader with {} spectra", reader.len());
 
-    let mut iter = reader.iter_spectra();
-    let spec = iter.next().unwrap();
-    drop(iter);
-
-    info!("{:?}", spec);
+    let spec = reader.get_spectrum(spectrum_idx).unwrap();
+    eprintln!("{:?}", spec);
 
     let (tic_time, tic_int) = reader.tic()?;
     let (tic_max_idx, tic_max) = tic_int
@@ -35,7 +35,7 @@ fn main() -> Result<(), MassLynxError> {
             },
         )
         .unwrap_or_default();
-    info!(
+    eprintln!(
         "TIC from {:0.2} to {:0.2} has maximum at {:0.2} with intensity {tic_max:0.2e}",
         tic_time.first().copied().unwrap_or_default(),
         tic_time.last().copied().unwrap_or_default(),
